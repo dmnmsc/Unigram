81d13c59839ee1293b84f0180beaba025068c066
dmnmsc <jpujob@gmail.com>
2017-03-30 20:58:49 +0200

    Hockeyapp thing

--- Unigram/Unigram/App.xaml.cs
+++ Unigram/Unigram/App.xaml.cs
@@ -83,10 +83,10 @@ namespace Unigram
             UnhandledException += async (s, args) =>
             {
                 args.Handled = true;
-                await new MessageDialog(args.Exception?.ToString() ?? string.Empty, "Unhandled exception").ShowQueuedAsync();
+                await new MessageDialog(args.Exception?.ToString() ?? string.Empty, "Unhandled exception").ShowAsync();
             };
 
-#if !DEBUG
+//#if !DEBUG
 
             HockeyClient.Current.Configure("7d36a4260af54125bbf6db407911ed3b",
                 new TelemetryConfiguration()
@@ -97,7 +97,7 @@ namespace Unigram
                                  WindowsCollectors.UnhandledException
                 });
 
-#endif
+//#endif
         }
 
         /////// <summary>
